d8e1813d7d71d31ff4c655678ecec1ca81b453c8
poppetex <poppetex21@gmail.com>
2021-09-19 11:46:48 -0400

    Added specialty teas - limited edition.

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ const menu = {
         order: 'Mango Lassi',
         cost: 0.9,
         etd: [2, 5]
+    },
+    'specialty-tea': {
+        order: 'Specialty Tea',
+        cost: 0.8,
+        etd: [1, 3]
     }
 }
 
@@ -63,19 +68,38 @@ function sendEmail(body){
 
 
 const form = document.getElementById("drink_form");
+const drinkTypeEl = document.getElementById("type_of_drink");
+
+
+drinkTypeEl.addEventListener('change', (e) => {
+    const specialTeasClass = document.getElementsByClassName('type_of_special_tea');
+    const specialTeasArray = Array.from(specialTeasClass);
+    if(e.target.value === 'specialty-tea') {        
+        specialTeasArray.forEach(el => { el.style.display = 'initial' });
+    } else {
+        specialTeasArray.forEach(el => { el.style.display = 'none' });
+    }
+    // specialTeasEl.style.display = 'initial';
+})
+
 
 form.addEventListener("submit", (event) => {
-    const drinkTypeEl = document.getElementById("type_of_drink");
+    // const drinkTypeEl = document.getElementById("type_of_drink");
     const amountEl = document.getElementById("amount");
     const nameEl = document.getElementById("order_by");
     const tipEl = document.getElementById("tip");
     const instructionsEl = document.getElementById("additional_instructions");
+    const specialTeasEl = document.getElementById("type_of_special_tea");
 
     const drinkType = drinkTypeEl.value;
     const amount = amountEl.value;
     const name = nameEl.value;
     const instructions = instructionsEl.value;
     const tip = !tipEl.value ? 0 : tipEl.value / 100;
+
+    //Limited ediition
+    const specialTeaType = specialTeasEl.value;
+    
 
     if(drinkType == 'blank') {
         alert("Please choose a drink.");
@@ -89,19 +113,20 @@ form.addEventListener("submit", (event) => {
         const preTip = perDrink * amt;
         const total = preTip * (1 + tip);
         return total.toFixed(2);
-    }    
+    }
 
     const totalCost = calculateCost(costPerDrink, amount, tip);
     document.getElementById("cost_span").innerHTML = totalCost;
 
     sendEmail(`
-    Name: ${name}. Type: ${drinkType}. Amount: ${amount}. Cost: $${totalCost}.
+    Name: ${name}. Type: ${drinkType}. Extra Type: ${specialTeaType}. Amount: ${amount}. Cost: $${totalCost}.
     Added Instructions: ${instructions}.
     `);
 
     const returnButton = document.getElementById("return_home");
     returnButton.style.display = 'initial';
 
-    alert(`You placed your order! Drink: ${menu[drinkType].order}. Amount ${amount}. Cost: $${totalCost}. Estimated delivery time: ${etd[0]}-${etd[1]}min.`);
+    alert(`You placed your order! Drink: ${menu[drinkType].order}. Amount ${amount}. Cost: $${totalCost}. 
+        Estimated delivery time: ${etd[0]}-${etd[1]}min or more depending on quantity.`);
     event.preventDefault();
 })
